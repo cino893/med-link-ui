498f76e2a0af7109a58d270f2d4f92f0d6b40498
Marcin Dębiec <debiec.marcin@gmail.com>
2019-05-21 20:09:32 +0200

    Working foreground service

---
+++ src/app/shared/foreground.service.android.ts
@@ -0,0 +1,63 @@
+@JavaProxy('com.tns.ForegroundService')
+export class ForegroundService extends android.app.Service {
+  public onCreate(): void {
+    super.onCreate();
+  }
+
+  public onDestroy(): void {
+    super.onDestroy();
+    this.stopForeground(true);
+  }
+
+  public onBind(param0: android.content.Intent): android.os.IBinder {
+    console.log(param0);
+    return null;
+  }
+
+  public onStartCommand(intent: android.content.Intent, flags: number, startId: number) {
+    super.onStartCommand(intent, flags, startId);
+    this.startForeground(1, this.createNotification(intent));
+    return android.app.Service.START_STICKY;
+  }
+
+  private createNotification(intent: android.content.Intent): android.app.Notification {
+    this.createNotificationChannel();
+    return this.getNotificationBuilder()
+      .setSmallIcon(android.R.drawable.btn_plus)
+      .setContentTitle(this.getTitle(intent))
+      .build();
+  }
+
+  private getNotificationBuilder() {
+    if (!android.support.v4.os.BuildCompat.isAtLeastO()) {
+      // Not Oreo, not creating notification channel as compatibility issues may exist
+      return new android.support.v4.app.NotificationCompat.Builder(this);
+    }
+
+    return new android.support.v4.app.NotificationCompat.Builder(this, 'TNS-ForegroundService-1');
+  }
+
+  private createNotificationChannel() {
+    if (!android.support.v4.os.BuildCompat.isAtLeastO()) {
+      // Not Oreo, not creating notification channel as compatibility issues may exist
+      return;
+    }
+    const importance = android.support.v4.app.NotificationManagerCompat.IMPORTANCE_LOW;
+    const mChannel = new android.app.NotificationChannel('TNS-ForegroundService-1', 'TNS-ForegroundService-1', importance);
+    var nm = this.getSystemService(android.content.Context.NOTIFICATION_SERVICE);
+    nm.createNotificationChannel(mChannel);
+  }
+
+  private getTitle(intent: android.content.Intent): string {
+    var title = intent.getStringExtra('title');
+    if (title) {
+      return title;
+    } else {
+      return 'Running in background';
+    }
+  }
+
+  public onStart(intent: android.content.Intent, startId: number) {
+    super.onStart(intent, startId);
+  }
+}
