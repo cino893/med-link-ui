3790804ddf7ee0281f6a97abccb78ef7a181da7a
Marcin Dębiec <debiec.marcin@gmail.com>
2019-09-21 12:53:25 +0200

    TraceWriterService - fixes after build

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,14 +1,12 @@
-import { HttpClient } from "@angular/common/http";
-import { identifierModuleUrl } from '@angular/compiler';
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { compose } from 'nativescript-email';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { alert } from "tns-core-modules/ui/dialogs";
-import { TextField } from "tns-core-modules/ui/text-field";
-import { nightScoutPath } from "~/app/env";
-import { DatabaseService } from '~/app/shared/database.service';
+import { alert } from 'tns-core-modules/ui/dialogs';
+import { TextField } from 'tns-core-modules/ui/text-field';
 import { DataFacadeService } from '~/app/shared/data-facade.service';
-import { sha1 } from 'sha1';
+import { DatabaseService } from '~/app/shared/database.service';
+import { TraceWriterService } from '~/app/shared/trace-writer.service';
 
 @Component({
   selector: "Search",
@@ -27,47 +25,82 @@ export class SearchComponent implements OnInit {
   pending = false;
   pumpData: string;
 
-  constructor(private changeDetectorRef: ChangeDetectorRef,
-              private databaseService: DatabaseService,
-              private dataFacadeService: DataFacadeService) {
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef,
+    private databaseService: DatabaseService,
+    private dataFacadeService: DataFacadeService,
+    private traceWriterService: TraceWriterService
+  ) {
     // Use the constructor to inject services.
   }
   ngOnInit(): void {
+    this.traceWriterService.subscribe(
+      ({ message, date, category, messageType }) => {
+        this.databaseService.insertLogs(date, message, messageType, category);
+      }
+    );
+
     this.databaseService.execSQLSuccessMonitor.subscribe(wynik => {
       if (wynik === undefined) {
-        this.pumpData = 'brak danych';
-      }
-      else {
-        this.pumpData = this.dataFacadeService.btData + 'SQL: ' + wynik;
+        this.pumpData = "brak danych";
+      } else {
+        this.pumpData = this.dataFacadeService.btData + "SQL: " + wynik;
       }
     });
-    this.sendDatatoNightscout7().then(() => console.log(this.nsUrl2 + "fffffffffffff3333333f"));
+    this.sendDatatoNightscout7().then(() =>
+      console.log(this.nsUrl2 + "fffffffffffff3333333f")
+    );
   }
+
+  sendLogs() {
+    this.databaseService.getLogs().subscribe(a => {
+      const aMaped = a.map(b => {
+        return b.reduce((prev, next) => prev + next, "");
+      });
+
+      const aReduced = aMaped.reduce((prev, next) => prev + next + "\r\n", "");
+
+      compose({
+        subject: "Debug med-link-ui",
+        body: aReduced,
+        to: ["jrkf@o2.pl"]
+      });
+    });
+  }
+
   Zapisz() {
     console.log("aaaaaa" + this.nsUrl);
-    const sha1 = require('sha1');
+    const sha1 = require("sha1");
     this.databaseService.insertNS(this.nsUrl, sha1(this.nsKey), this.nsKey);
     this.pumpData = this.dataFacadeService.btData;
     //this.databaseService.updateNS("adsad", "1231231");
-    console.log("NS URL: " + this.nsUrl + ' ddddddddddd ' + this.nsKey);
-    this.sendDatatoNightscout6().then(() => console.log(this.slowo + "aRRRRRRRRRR"));
-    if (this.nsUrl.substring(0, 8).toUpperCase() !== 'HTTPS://' || this.nsUrl.substring(this.nsUrl.length - 1, this.nsUrl.length) === '/') {
-      this.slowo2 = 'ZŁY ADRES URL !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!';
-    }
-    else {
-      this.slowo2 = 'OK! ';
+    console.log("NS URL: " + this.nsUrl + " ddddddddddd " + this.nsKey);
+    this.sendDatatoNightscout6().then(() =>
+      console.log(this.slowo + "aRRRRRRRRRR")
+    );
+    if (
+      this.nsUrl.substring(0, 8).toUpperCase() !== "HTTPS://" ||
+      this.nsUrl.substring(this.nsUrl.length - 1, this.nsUrl.length) === "/"
+    ) {
+      this.slowo2 = "ZŁY ADRES URL !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+    } else {
+      this.slowo2 = "OK! ";
     }
   }
   sendDatatoNightscout6() {
     return new Promise((resolve, reject) => {
       this.getNSData().subscribe(g => {
-         g.map(bol => {
-           console.log(bol.http.toString() + "JJJJJJJ" + bol.secret.toString());
-           this.slowo = this.slowo2 + '  ' + bol.http.toString() + ' ' +  bol.secret.toString();
-         });
-         console.log("as" + this.slowo);
-         resolve(),
-        reject();
+        g.map(bol => {
+          console.log(bol.http.toString() + "JJJJJJJ" + bol.secret.toString());
+          this.slowo =
+            this.slowo2 +
+            "  " +
+            bol.http.toString() +
+            " " +
+            bol.secret.toString();
+        });
+        console.log("as" + this.slowo);
+        resolve(), reject();
       });
     });
   }
@@ -75,21 +108,22 @@ export class SearchComponent implements OnInit {
     return new Promise((resolve, reject) => {
       this.getNSData().subscribe(g => {
         g.map(bol => {
-          console.log(bol.http.toString() + "66666666666" + bol.secret.toString());
+          console.log(
+            bol.http.toString() + "66666666666" + bol.secret.toString()
+          );
           this.nsUrl2 = bol.http.toString();
           this.nsKey2 = bol.hash.toString();
         });
         console.log("as" + this.nsUrl2);
-        resolve(),
-          reject();
+        resolve(), reject();
       });
     });
   }
-  checkUrl (nsUrl: string){
-    if (this.nsUrl.substring(0, 8).toUpperCase() === 'HTTPS://') {
-      this.slowo = 'zly adres';
+  checkUrl(nsUrl: string) {
+    if (this.nsUrl.substring(0, 8).toUpperCase() === "HTTPS://") {
+      this.slowo = "zly adres";
     }
-    console.log(this.slowo + 'TTTTTTTTT');
+    console.log(this.slowo + "TTTTTTTTT");
   }
   setNS(arg) {
     console.log("setttNS");
@@ -100,9 +134,10 @@ export class SearchComponent implements OnInit {
     console.log("setttNSUURRL");
     console.log(arg.text);
     this.nsKey = arg.text;
-
   }
-  getNSData(): Observable<Array<{ http: string; secret: string; hash: string }>> {
+  getNSData(): Observable<
+    Array<{ http: string; secret: string; hash: string }>
+  > {
     return this.databaseService.NSconf().pipe(
       map(rows => {
         return rows.map(a => ({
